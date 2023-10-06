d93917c9f67bf58b495b38fca9aca1ba33d538cb
Lucas Serral <lucasser21@gmail.com>
2023-10-05 21:14:50 -0300

    fixes

--- TP 1/Commands.sh
+++ TP 1/Commands.sh
@@ -1,26 +1,26 @@
 #punto a, b, c
 # creamos un archivo con los puntos a y b -> punto c
-sed -r 's/\.[^\n]/\.\n/g;  /^ *$/d' paraprobar.txt > breve_historia_2.txt
+sed -r 's/\.[^\n]/\.\n/g;  /^ *$/d' breve_historia.txt > breve_historia_2.txt
 
 # punto d
 # (Como indicaba que listara las oraciones desde el archivo breve_historia.txt y no permitía usar el generado en el punto anterior, para encontrar las oraciones, separamos las oraciones en líneas redireccionamos su salida al grep que busca independencia, ignorando mayúsculas y minúsculas)
-sed -r 's/\.[^\n]/\.\n/g;' paraprobar.txt | grep independencia -i
+sed -r 's/\.[^\n]/\.\n/g;' breve_historia.txt | grep independencia -i
 
 # punto e
-grep '^El.*\.$' paraprobar.txt
+grep '^El.*\.$' breve_historia.txt
 
 # punto f
-sed -r 's/\.[^\n]/\.\n/g;' paraprobar.txt | grep -c '[^(peronismo)]*peronismo.*\.'
+sed -r 's/\.[^\n]/\.\n/g;' breve_historia.txt | grep -c '[^(peronismo)]*peronismo.*\.'
 
 # punto g
-sed -r 's/\.[^\n]/\.\n/g;' paraprobar.txt | grep -cE '(Sarmiento).*(Rosas)|(Rosas).*(Sarmiento)'
+sed -r 's/\.[^\n]/\.\n/g;' breve_historia.txt | grep -cE '(Sarmiento).*(Rosas)|(Rosas).*(Sarmiento)'
 
 # punto h
-sed -r 's/\.[^\n]/\.\n/g;' paraprobar.txt | grep  '^.*18[0-9][0-9].*\.$'
+sed -r 's/\.[^\n]/\.\n/g;' breve_historia.txt | grep -E '^.*(18(([0-9][1-9])|([1-9][0-9]))|1900).*\.$'
 
 # punto i
 # Como veníamos trabajando sobre el archivo original "breve_historia.txt", entendemos que la sustitución se debe de realizar en este mismo archivo.
-sed 's/^[a-zA-Z]*\b//g' paraprobar.txt
+sed 's/^[a-zA-Z]*\b//g' breve_historia.txt > breve_historia.txt
 
 # punto j
 echo -e '\n\nArchivos ".txt" enumerados:'
